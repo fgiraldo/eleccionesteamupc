8eafeeb75f3c5da0949e5bd92643610bb9f41915
nestor <iluvtr@gmail.com>
2017-07-15 21:42:50 -0500

    1

---
+++ TestEleccionesWS/ResultadoVotacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace TestEleccionesWS
+{
+    [DataContract]
+    public class ResultadoVotacion
+    {
+
+        [DataMember]
+        public int IdMilitante { get; set; }
+
+        [DataMember]
+        public string NombreCandidato { get; set; }
+
+        [DataMember]
+        public int CantVotos { get; set; } 
+
+    }
+}
---
+++ TestEleccionesWS/ResultadosVotacionServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TestEleccionesWS
+{
+    [TestClass]
+    public class ResultadosVotacionServiceTest
+    {
+        [TestMethod]
+        public void DeberiaNoPermitirAccesoCuandoNoHayActasRegistradas()
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            String distritoSinActasRegistradas = "20";
+            HttpWebRequest request = (HttpWebRequest)WebRequest
+                .Create("http://localhost:56837/ResultadosVotacionService.svc/resultados-votacion?idDepartamento=15&idProvincia=15&idDistrito="+distritoSinActasRegistradas);
+            request.Method = "GET";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Assert.Fail("Debería haber arrojado una excepción");
+            }
+            catch (WebException e)
+            { 
+                Assert.IsTrue(e.Message.Contains("401"));
+            }                   
+        }
+
+        [TestMethod]
+        public void DeberiaRetornarResultados()
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            String distritoConActasRegistradas = "15";
+            HttpWebRequest request = (HttpWebRequest)WebRequest
+                .Create("http://localhost:56837/ResultadosVotacionService.svc/resultados-votacion?idDepartamento=15&idProvincia=15&idDistrito=" + distritoConActasRegistradas);
+            request.Method = "GET";
+            
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream() );
+            String tramaJson = reader.ReadToEnd();
+            List<ResultadoVotacion> resultados = js.Deserialize<List<ResultadoVotacion>>(tramaJson); 
+            Assert.IsTrue(resultados.Count > 0);
+           
+        }
+
+
+
+    }
+}
