2b3cc179107cabb984cec610a0f79a5abe63b7c5
nestor <iluvtr@gmail.com>
2017-07-15 23:04:51 -0500

    1

--- EleccionesWS/MesaService.svc.cs
+++ EleccionesWS/MesaService.svc.cs
@@ -6,9 +6,7 @@ using System.ServiceModel;
 using System.Text;
 
 namespace EleccionesWS
-{
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MesaService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select MesaService.svc or MesaService.svc.cs at the Solution Explorer and start debugging.
+{ 
     public class MesaService : IMesaService
     { 
 
---
+++ EleccionesWS/ResultadoVotacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace EleccionesWS
+{
+    [DataContract]
+    public class ResultadoVotacion
+    {
+
+        [DataMember]
+        public int IdMilitante { get; set; }
+
+
+        [DataMember]
+        public string NombreCandidato { get; set; }     
+
+        [DataMember]
+        public int CantVotos { get; set; } 
+
+    }
+}
